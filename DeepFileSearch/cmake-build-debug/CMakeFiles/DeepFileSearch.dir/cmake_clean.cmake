dd95b294e4a4f47cf5f43afddeb24454319db27b
Devin Wallace <devinwallace@protonmail.com>
2020-09-23 22:36:16 -0500

    init

---
+++ DeepFileSearch/cmake-build-debug/CMakeFiles/DeepFileSearch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeepFileSearch.dir/main.cpp.obj"
+  "DeepFileSearch.exe"
+  "DeepFileSearch.exe.manifest"
+  "DeepFileSearch.lib"
+  "DeepFileSearch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeepFileSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
